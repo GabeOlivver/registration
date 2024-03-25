792e1acac171a7b3fcd01a9fc725f0aa74f80316
GabeOlivver <gabriel.oliveira294@fatec.sp.gov.br>
2024-03-24 22:44:33 -0300

    estrutura estudantes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { Student } from '../student';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  
+  student: Student[] = [ 
+    {enrolment:1, name:"Gabriel", course:"computer engineer"},
+    {enrolment:2, name:"Nina", course:"english"},
+    {enrolment:3, name:"Larry the Bird", course:"Seeds"},
+  ];
 
 }
---
+++ src/app/student.ts
@@ -0,0 +1,5 @@
+export interface Student{
+    enrolment: number,
+    name: string,
+    course: string
+}
